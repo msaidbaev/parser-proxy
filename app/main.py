ae488e3f9aa9f8aeb094d53e2d24f8df1fd46228
james <smr01234585@gmail.com>
2023-05-01 03:13:52 +0500

    Revert "fix fastapi model, add view form"
    This reverts commit 02bff10e9d92e8819d67b645706171b96baeacea.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-import json
 from xgboost import XGBClassifier
 
 app = FastAPI()
 
 
 @app.get('/model')
-async def predict(data: str):
-    data = json.loads(data)
-    print(data)
-    print(type(data))
+def predict():
     classifier = XGBClassifier()
     classifier.load_model('/model/app/model.xgb')
-    prediction = classifier.predict([data]).tolist()
+    prediction = classifier.predict([[1, 1, 1, 1, 0, 0, 0, 0, 2, 15, 427500.0, 32, 12.0, 4,	1, 0, 4, 18]]).tolist()
     return prediction[0]
