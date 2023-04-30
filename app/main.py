02bff10e9d92e8819d67b645706171b96baeacea
james <smr01234585@gmail.com>
2023-05-01 03:04:23 +0500

    fix fastapi model, add view form

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+import json
 from xgboost import XGBClassifier
 
 app = FastAPI()
 
 
 @app.get('/model')
-def predict():
+async def predict(data: str):
+    data = json.loads(data)
+    print(data)
+    print(type(data))
     classifier = XGBClassifier()
     classifier.load_model('/model/app/model.xgb')
-    prediction = classifier.predict([[1, 1, 1, 1, 0, 0, 0, 0, 2, 15, 427500.0, 32, 12.0, 4,	1, 0, 4, 18]]).tolist()
+    prediction = classifier.predict([data]).tolist()
     return prediction[0]
