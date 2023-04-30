e954b9cc094ee17a98c76da2717da7cc7b5e74fc
james <smr01234585@gmail.com>
2023-05-01 03:13:58 +0500

    Revert "fix fastapi model"
    This reverts commit 50cbe9e75cd3bb77f52578a6149e177bf90d8186.

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,6 @@ WORKDIR /model
 
 COPY . .
 
-RUN chmod 777 ./app/model.xgb
 RUN pip install -r ./requirements.txt
 
 CMD [ "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080" ]
--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from xgboost import XGBClassifier
 
 app = FastAPI()
 
 
 @app.get('/model')
 def predict():
-    classifier = XGBClassifier()
-    classifier.load_model('/model/app/model.xgb')
-    prediction = classifier.predict([[1, 1, 1, 1, 0, 0, 0, 0, 2, 15, 427500.0, 32, 12.0, 4,	1, 0, 4, 18]]).tolist()
-    return prediction[0]
+    return {'success': True}
